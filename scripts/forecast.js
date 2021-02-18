58bc00c7f778688cb40d4875147bde21081f815a
mahovsson <matus137@gmail.com>
2021-02-18 08:33:54 +0100

    weather app init

---
+++ scripts/forecast.js
@@ -0,0 +1,28 @@
+const key = '2FCOTopSWdF0WDR3wHiYQuRH6gZ9rEh5';
+
+// get weather condition
+
+const getWeather = async (id) => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+}
+
+// get city info
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+}
