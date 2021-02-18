3c93051d31acf7d976ac5378c3ddc13adb7825ef
mahovsson <59553868+mahovsson@users.noreply.github.com>
2021-02-18 11:19:18 +0100

    Add files via upload

---
+++ scripts/forecast.js
@@ -0,0 +1,28 @@
+const key = '6jwmeAc0WDAjFoLyyGRe7FFe8ZVNiMfl';
+
+// get weather condition
+
+const getWeather = async (id) => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+}
+
+// get city info
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+}
